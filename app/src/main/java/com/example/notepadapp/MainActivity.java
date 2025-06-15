d76318b6eccdb8bfdd0ccfca16f68f54c00b168a
outx <outingxiao@126.com>
2025-06-15 12:04:15 +0800

    完成首页显示和长按删除

--- app/src/main/java/com/example/notepadapp/MainActivity.java
+++ app/src/main/java/com/example/notepadapp/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.notepadapp;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.notepadapp.bean.Notepad;
+import com.example.notepadapp.db.MyDbHelper;
 import com.example.notepadapp.ui.EditActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    // 日记列表
+    private RecyclerView listRV;
+
+    // 数据库帮助类
+    private MyDbHelper dbHelper;
+
+    NotepadListAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +49,101 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        dbHelper=new MyDbHelper(this);
+
         findViewById(R.id.addIv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, EditActivity.class));
             }
         });
+
+        listRV = findViewById(R.id.listRv);
+        adapter = new NotepadListAdapter();
+        listRV.setAdapter(adapter);
+        // 添加横线
+        listRV.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Notepad> notepads = dbHelper.findAll();
+        adapter.updateList(notepads);
+    }
+
+    class NotepadListAdapter extends RecyclerView.Adapter<NotepadListAdapter.ViewHolder> {
+
+        private final List<Notepad> notepads=new ArrayList<>();
+
+
+        @SuppressLint("NotifyDataSetChanged")
+        public void updateList(List<Notepad> list){
+            notepads.clear();
+            notepads.addAll(list);
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view=View.inflate(MainActivity.this,R.layout.item_list,null);
+            return new ViewHolder(view);
+        }
+
+        // 设置数据
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Notepad notepad = notepads.get(position);
+            holder.content.setText(notepad.getContent());
+            holder.time.setText(notepad.getTime());
+
+            // 长按删除
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    showDeleteDialog(notepad);
+                    return false;
+                }
+            });
+        }
+
+        private void showDeleteDialog(Notepad notepad){
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("温馨提示")
+                    .setMessage("确定要删除记录吗？")
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "已取消删除！", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dbHelper.delete(notepad);
+                            Toast.makeText(MainActivity.this, "删除成功！", Toast.LENGTH_SHORT).show();
+                            updateList(dbHelper.findAll());
+                        }
+                    })
+                    .create();
+            alertDialog.show();
+        }
+
+        @Override
+        public int getItemCount() {
+            return notepads.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            TextView content,time;
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                content = itemView.findViewById(R.id.contentTv);
+                time = itemView.findViewById(R.id.timeTv);
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/notepadapp/db/MyDbHelper.java
+++ app/src/main/java/com/example/notepadapp/db/MyDbHelper.java
@@ -1,12 +1,17 @@
 package com.example.notepadapp.db;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 import com.example.notepadapp.bean.Notepad;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyDbHelper extends SQLiteOpenHelper {
     private static final int version =1;
@@ -27,8 +32,35 @@ public class MyDbHelper extends SQLiteOpenHelper {
 
     }
 
+    // 插入记录
     public void insertNote(Notepad notepad){
         String insertSql = "insert into notepad(content,time) values (?,?)";
         getWritableDatabase().execSQL(insertSql,new String[]{notepad.getContent(),notepad.getTime()});
+    }
+
+    // 查询所有记录
+    @SuppressLint("Range")
+    public List<Notepad> findAll(){
+        SQLiteDatabase db = getWritableDatabase();
+        String sql = "select * from notepad";
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<Notepad> notepads=new ArrayList<>();
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex("id"));
+            String content = cursor.getString(cursor.getColumnIndex("content"));
+            String item = cursor.getString(cursor.getColumnIndex("time"));
+            Notepad notepad=new Notepad(id,content,item);
+            notepads.add(notepad);
+        }
+
+        return notepads;
+    }
+
+    // 删除记录
+    public void delete(Notepad notepad){
+        String sql = "delete from notepad where id=?";
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sql,new Object[]{notepad.getId()});
     }
 }
